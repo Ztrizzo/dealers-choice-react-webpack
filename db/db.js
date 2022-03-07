364df0607b32db631ac7bc51bc8a7b64646f8fb3
Zach Rizzo <RizzoZachary2@gmail.com>
2022-03-07 11:01:48 -0600

    changed random tasks to look more like tasks using faker

--- db/db.js
+++ db/db.js
@@ -24,10 +24,47 @@ const Task = sequelize.define('Task', {
 
 
 Task.createRandom = async() => {
+let heading;
+let description;
+let seed = faker.datatype.number({
+  'min': 1,
+  'max': 5
+});
+
+if(seed === 1){
+  heading = `Purchase ${faker.commerce.color()} paint.`
+  description = `${faker.name.firstName()} wants me to buy paint for ${faker.company.companyName()}
+  so that they can be ${faker.company.bsAdjective()} and meet their goal of being a ${faker.company.catchPhrase().toLowerCase()}.`;
+}
+else if (seed === 2){
+  heading = `Call ${faker.name.firstName()}`
+  description = `Need to talk to ${faker.name.lastName()}
+  to decide what ${faker.name.jobType()} to ${faker.company.bsBuzz()} the project.`
+
+}
+else if (seed === 3){
+  heading = `Schedule meeting with ${faker.name.firstName()} and ${faker.name.firstName()}`;
+  description = `Schedule a meeting to ${faker.company.bsBuzz()} the addition of a ${faker.company.catchPhraseNoun()}
+  in order to reduce ${faker.company.bsAdjective()} ${faker.company.catchPhraseNoun()}.`
+}
+else if (seed === 4){
+  heading = `Review production of ${faker.commerce.productName()}`;
+  description = `Check the efficiency of ${faker.commerce.productAdjective()} vs ${faker.commerce.productAdjective()}
+  when manufacturing these in ${faker.address.country()}.`
+  
+}
+else{
+  heading = `${faker.hacker.verb()} the ${faker.hacker.noun()}`;
+  description = faker.hacker.phrase();
+  
+}
+
+
+
   const newTask = await Task.create({
-    heading: faker.lorem.sentence(5),
+    heading: heading,
     dueDate: faker.date.future(),
-    description: faker.lorem.paragraph(1),
+    description: description,
     importance: faker.datatype.number({
       'min': 1,
       'max': 5
